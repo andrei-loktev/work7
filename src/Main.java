e51b8e6290f4ee98fcc87d0e9be0c533c442d6da
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-16 19:57:10 +0300

    сделано 5-7 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
@@ -12,12 +10,27 @@ public class Main {
         }
         //task3
         System.out.println("задание 3");
-        for (int i = 0; i<= 17;  i = i + 2) {
+        for (int i = 0; i <= 17; i = i + 2) {
             System.out.println(i);
         }
         //task4
         System.out.println("задание 4");
         for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        //task5
+        System.out.println("задание 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + "год является високосным");
+        }
+        //task6
+        System.out.println("задание 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        //task7
+        System.out.println("задание 7");
+        for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
     }
