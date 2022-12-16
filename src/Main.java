56dbe2228562ecf94ead9bd534c145b7f3967126
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2022-12-16 19:32:43 +0300

    сделано 4 задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //task2
+        System.out.println("задание 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        //task3
+        System.out.println("задание 3");
+        for (int i = 0; i<= 17;  i = i + 2) {
+            System.out.println(i);
+        }
+        //task4
+        System.out.println("задание 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
 }
